84282f235751bdf901378e745dc1716c8edcfb3d
Daniel Hristov <daniel.hristov@mail.bg>
2014-12-13 11:20:26 +0200

    Cleanup

--- dbcompare/src/name/dido/dbcompare/compare/cmd/IOrderByClauseProvider.java
+++
@@ -1,7 +0,0 @@
-package name.dido.dbcompare.compare.cmd;
-
-import java.sql.SQLException;
-
-public interface IOrderByClauseProvider {
-	public String getOrderByClause(String tableName) throws MetadataOrderByProviderException, SQLException;
-}
--- dbcompare/src/name/dido/dbcompare/tablelistprovider/ITableListProvider.java
+++
@@ -1,8 +0,0 @@
-package name.dido.dbcompare.tablelistprovider;
-
-import java.sql.SQLException;
-import java.util.List;
-
-public interface ITableListProvider {
-	public List<String> getTableNames() throws SQLException;
-}
